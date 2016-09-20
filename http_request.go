2a9c6cb4b49ec661eba84ef3bd936fd32ee2fe00
Richard Tuin <richardtuin@gmail.com>
2016-09-20 16:17:55 +0200

    Introduce http successful and client error functions

--- http_request.go
+++ http_request.go
@@ -11,5 +11,23 @@ func IsHTTPServerError(r *http.Response) bool {
 		return true
 	}
 
-	return r.StatusCode >= 500 && r.StatusCode <= 600
+	return r.StatusCode >= 500 && r.StatusCode < 600
+}
+
+// IsHTTPClientError checks whether the provided response indicates a client error
+func IsHTTPClientError(r *http.Response) bool {
+	if reflect.DeepEqual(http.Response{}, *r) {
+		return false
+	}
+
+	return r.StatusCode >= 400 && r.StatusCode < 500
+}
+
+// IsHTTPSuccessful checks whether the provided response indicates a successful response
+func IsHTTPSuccessful(r *http.Response) bool {
+	if reflect.DeepEqual(http.Response{}, *r) {
+		return false
+	}
+
+	return r.StatusCode >= 200 && r.StatusCode < 300
 }
--- http_request_test.go
+++ http_request_test.go
@@ -36,3 +36,63 @@ func TestIsHTTPServerError(t *testing.T) {
 		assert.Equal(t, data[d].Result, IsHTTPServerError(&resp))
 	}
 }
+
+func TestIsHTTPClientError(t *testing.T) {
+	data := []struct {
+		StatusCode int
+		Result     bool
+	}{
+		{0, false},
+		{http.StatusOK, false},
+		{http.StatusCreated, false},
+		{http.StatusNoContent, false},
+		{http.StatusMovedPermanently, false},
+		{http.StatusFound, false},
+		{http.StatusBadRequest, true},
+		{http.StatusForbidden, true},
+		{http.StatusUnauthorized, true},
+		{http.StatusUnsupportedMediaType, true},
+		{http.StatusInternalServerError, false},
+		{http.StatusBadGateway, false},
+		{http.StatusGatewayTimeout, false},
+		{http.StatusNotImplemented, false},
+	}
+
+	for d := range data {
+		var resp http.Response
+		if data[d].StatusCode != 0 {
+			resp = http.Response{StatusCode: data[d].StatusCode}
+		}
+		assert.Equal(t, data[d].Result, IsHTTPClientError(&resp))
+	}
+}
+
+func TestIsHTTPSuccessful(t *testing.T) {
+	data := []struct {
+		StatusCode int
+		Result     bool
+	}{
+		{0, false},
+		{http.StatusOK, true},
+		{http.StatusCreated, true},
+		{http.StatusNoContent, true},
+		{http.StatusMovedPermanently, false},
+		{http.StatusFound, false},
+		{http.StatusBadRequest, false},
+		{http.StatusForbidden, false},
+		{http.StatusUnauthorized, false},
+		{http.StatusUnsupportedMediaType, false},
+		{http.StatusInternalServerError, false},
+		{http.StatusBadGateway, false},
+		{http.StatusGatewayTimeout, false},
+		{http.StatusNotImplemented, false},
+	}
+
+	for d := range data {
+		var resp http.Response
+		if data[d].StatusCode != 0 {
+			resp = http.Response{StatusCode: data[d].StatusCode}
+		}
+		assert.Equal(t, data[d].Result, IsHTTPSuccessful(&resp))
+	}
+}
